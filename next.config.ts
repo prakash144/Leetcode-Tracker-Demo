c52158c590435c80063894dfc7fbf071c822d915
prakash144 <prakash.rabidas1@gmail.com>
2025-04-12 02:28:28 +0530

    Deployement changes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    // Optional: Change the output directory `out` -> `dist`
-    distDir: 'dist',
+
+    // Required for GitHub Pages deployment
+    basePath: '/Leetcode-Tracker',
+    assetPrefix: '/Leetcode-Tracker',
+
+    images: {
+        unoptimized: true, // disable image optimization for static hosting
+    },
 };
 
 export default nextConfig;
