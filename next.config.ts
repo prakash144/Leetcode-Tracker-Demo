4b9c517360b3e650b7c50471356c51dfbc6b6f0d
prakash144 <prakash.rabidas1@gmail.com>
2025-04-12 02:13:49 +0530

    Deployement

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'export',
     // Optional: Change the output directory `out` -> `dist`
-    distDir: 'out',
+    distDir: 'dist',
 };
 
 export default nextConfig;
